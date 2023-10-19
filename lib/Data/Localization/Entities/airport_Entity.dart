eb74af8a12b8c8a9efed614dfc77aebe069008d5
Dharti Chauhan <dhartic.itpath@gmail.com>
2023-10-19 19:53:02 +0530

    Isar Database added

--- lib/Data/Localization/Entities/airport_Entity.dart
+++ lib/Data/Localization/Entities/airport_Entity.dart
@@ -1,3 +1,24 @@
+import 'package:isar/isar.dart';
+
+part 'airport_Entity.g.dart';
+
+@Collection()
+class AirportEntities{
+  Id id = Isar.autoIncrement;
+  String? icao;
+  String? iata;
+  String? name;
+  String? city;
+  String? state;
+  String? country;
+   double? elevation;
+  double? lat;
+  double? lon;
+  String? tz;
+}
+
+
+
 // import 'package:isar/isar.dart';
 //
 // part 'airport_Entity.g.dart';
